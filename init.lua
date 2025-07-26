406941f3eca6683e24c2c1596e93a78d3d77e323
Shawn Debnath <shawn@debnath.net>
2025-07-26 16:56:14 -0700

    Add support Neotree

--- init.lua
+++ init.lua
@@ -756,7 +756,41 @@ require('lazy').setup({
       }
     end,
   },
-
+  { -- neo-tree
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      'nvim-tree/nvim-web-devicons', -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
+    config = function()
+      require('neo-tree').setup {
+        close_if_last_window = true,
+        popup_border_style = 'rounded',
+        enable_git_status = true,
+        enable_diagnostics = false,
+        default_component_configs = {
+          indent = {
+            padding = 1,
+          },
+        },
+        window = {
+          position = 'float', -- this is what makes it telescope-like
+          width = 40,
+        },
+        filesystem = {
+          filtered_items = {
+            visible = true,
+            hide_dotfiles = false,
+            hide_gitignored = false,
+          },
+        },
+      }
+    end,
+    vim.keymap.set('n', '<leader>e', ':Neotree toggle float<CR>', { desc = 'Toggle Neo-tree (float)' }),
+  },
   { -- Autoformat
     'stevearc/conform.nvim',
     event = { 'BufWritePre' },
